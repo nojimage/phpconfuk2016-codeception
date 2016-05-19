7658b7f9374357efcd51d68ec83ffad0fca918e2
nojimage <nojimage@gmail.com>
2016-05-19 22:43:54 +0900

    Twitter検索のサンプル

---
+++ tests/acceptance/TwitterSearchCept.php
@@ -0,0 +1,20 @@
+<?php
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('ツイッターで #phpconfuk を検索できる');
+
+$I->amOnPage('/');
+$I->comment('検索フィールドを開いて入力');
+$I->click('検索');
+
+$I->waitForElementVisible('#search-query');
+$I->fillField('#search-query', '#phpconfuk');
+$I->click('.StreamsTopBar-searchButton');
+$I->wait(3);
+
+$I->expect('検索ページが開いている');
+$I->seeCurrentUrlMatches('~^/search~');
+$I->seeInTitle('#phpconfuk');
+
+$I->expect('結果に"プリンさんすごい！"を含んでいる');
+$I->see('プリンさんすごい！', '#timeline');

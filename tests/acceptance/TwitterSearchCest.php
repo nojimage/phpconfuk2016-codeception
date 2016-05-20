6c51bc78bb3f2c119e2c0e0d8bf6225629d710ad
nojimage <nojimage@gmail.com>
2016-05-20 10:19:14 +0900

    Cest形式を追加

---
+++ tests/acceptance/TwitterSearchCest.php
@@ -0,0 +1,41 @@
+<?php
+
+class TwitterSearchCest
+{
+
+    public function _before(AcceptanceTester $I)
+    {
+
+    }
+
+    public function _after(AcceptanceTester $I)
+    {
+
+    }
+
+    /**
+     *
+     * @param AcceptanceTester $I
+     * @group twitter
+     */
+    public function tryToTest(AcceptanceTester $I)
+    {
+        $I->wantTo('ツイッターで #phpconfuk を検索できる');
+
+        $I->amOnPage('/');
+        $I->comment('検索フィールドを開いて入力');
+        $I->click('検索');
+
+        $I->waitForElementVisible('#search-query');
+        $I->fillField('#search-query', '#phpconfuk');
+        $I->click('.StreamsTopBar-searchButton');
+        $I->wait(3);
+
+        $I->expect('検索ページが開いている');
+        $I->seeCurrentUrlMatches('~^/search~');
+        $I->seeInTitle('#phpconfuk');
+
+        $I->expect('結果に"プリンさんすごい！"を含んでいる');
+        $I->see('プリンさんすごい！', '#timeline');
+    }
+}
